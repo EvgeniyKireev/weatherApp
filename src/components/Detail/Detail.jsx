dfbdcaa68a0993801726af1df57676ceac7d4668
eeeboooooy <zheka.mtb@gmail.com>
2020-11-23 14:50:02 +0300

    change README.md

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -2,23 +2,18 @@ import {useParams} from 'react-router-dom';
 import React, {useEffect, useState} from "react"
 import {Loader} from "../../common/Loader";
 import s from "../../styles/Detail.module.css";
-import {Map, Placemark, YMaps} from "react-yandex-maps";
-
+import {Map, YMaps} from "react-yandex-maps";
+import NotInterestedIcon from '@material-ui/icons/NotInterested';
 const apikey = 'e60827ffd1072fb3817125f07e132e5c';
 export const Detail = () => {
     const mapData = {
         center: [55.751574, 37.573856],
         zoom: 2,
     };
-    const [coords, setCoords] = useState({latitude: '', longitude: '', name: ''});
     const city = useParams().city;
     let [infoCity, setInfo] = useState({temp: ''});
     let [loading, setLoading] = useState(false);
     useEffect(async () => {
-        navigator.geolocation.getCurrentPosition(showPosition); // Запрашиваем местоположение, и в случае успеха вызываем функцию showPosition
-        function showPosition(position) {
-            setCoords({latitude: position.coords.latitude, longitude: position.coords.longitude})
-        }
         if (city) {
             setLoading(true)
             try {
@@ -38,18 +33,20 @@ export const Detail = () => {
 
             setLoading(false)
         }
-    }, [city, infoCity.temp]);
+    }, [city, infoCity.name, infoCity.temp]);
     if (loading) {
         return <div className={'loader'}><Loader /></div>
     }
 
     return <div>
+        {!infoCity.name && <h1 className={s.notFound}><div><NotInterestedIcon style={{fontSize: '100px'}} /></div>
+            404 NOT FOUND</h1>}
         {infoCity.temp && <div className={s.detail}>
             <div> <h1>{infoCity.name}</h1>
                 <div><div className={s.allTemp}>Temperature:<span>&nbsp;{infoCity.temp} °C</span></div>
                     <div className={s.allTemp}>Minimum temperature tomorrow:<span>&nbsp;{infoCity.min} °C</span></div>
                     <div className={s.allTemp}>Maximum temperature tomorrow:<span>&nbsp;{infoCity.max} °C</span></div>
-                    <div className={s.allTemp}>Wind speed:<span>&nbsp;{infoCity.wind} °C</span></div>
+                    <div className={s.allTemp}>Wind speed:<span>&nbsp;{infoCity.wind}</span></div>
                     <div className={s.allTemp}><span>{infoCity.desc}</span></div></div></div>
             <div id="map"><YMaps>
                 <Map className={s.map} defaultState={mapData}>
